5de8a99c374a8383d36fdc7cc65e105ac50f474a
luigipsmf <luigipsmf@gmail.com>
2025-08-25 20:08:52 -0300

    Mensaje de amor

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ console.log("Mi nombre es " + nombreCompleto + ", tengo " + edad + " años, nac�
 console.log("En 5 años tendré " + (edad + 5) + " años.");
 alert("Mi nombre es " + nombreCompleto + ", tengo " + edad + " años, nací en el año " + FNacimiento + ", soy " + estadoCivil + " y es " + masculino + " que soy hombre.");
 let amor=prompt("¿Cuál es tu estado civil?");
-if amor=="Casado" || amor=="casado" || amor=="CASADO"{
+if (amor=="Casado"|| amor=="casado" || amor=="CASADO") {
     alert("Felicidades por tu matrimonio");
-} else if amor=="Soltero" || amor=="soltero" || amor=="SOLTERO"{
+} else if (amor=="Soltero" || amor=="soltero" || amor=="SOLTERO"){
     alert("Suerte en el amor");
 } else {
     alert("Estado civil no reconocido");
